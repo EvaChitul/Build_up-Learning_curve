3858745fb35b38a946948a93830355323a059c7d
evachitul <eva.chitul@coleurope.eu>
2020-09-14 13:52:23 +0200

    '''Take a list and write a program that prints out all the elements of the list that are less than 5. Instead of printing them one by one make a new list that has all the elements less than 5 and print that list'''

--- less_than_ten.py
+++ less_than_ten.py
@@ -2,8 +2,12 @@
 make a new list that has all the elements less than 5 and print that list'''
 
 list_to_use = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+new_list = []
 
 for i in list_to_use:
     if i < 5:
         print (i)
+        new_list.append(i)
+print (new_list)
+
 
